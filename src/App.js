d9043e88b96e7f88975009048636a6ccde227fa1
d4ydr34mzzz <d4y.dr34mzzz@gmail.com>
2019-05-27 22:07:49 -0700

    added Header component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Header from "./header/Header";
 
 function App() {
   console.log(process.env.REACT_APP_TMDB_API_KEY);
   return (
     <div>
-      <h1>Sweet Pumpkins</h1>
+      <Header />
     </div>
   );
 }
