5e62967bbae399ff00e4a347138899eb6319f829
d4ydr34mzzz <d4y.dr34mzzz@gmail.com>
2019-05-27 22:29:29 -0700

    added Navigation, Movies, and Main components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Header from "./header/Header";
+import Main from "./main/Main";
 
 function App() {
   console.log(process.env.REACT_APP_TMDB_API_KEY);
   return (
     <div>
       <Header />
+      <Main />
     </div>
   );
 }
