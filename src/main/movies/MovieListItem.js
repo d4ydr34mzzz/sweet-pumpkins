6b6dd6108bf7ec7930d61d53e2fc6c665ddd733c
d4ydr34mzzz <d4y.dr34mzzz@gmail.com>
2019-06-01 16:16:12 -0700

    added styling to movies section

--- src/main/movies/MovieListItem.js
+++ src/main/movies/MovieListItem.js
@@ -2,13 +2,26 @@ import React from "react";
 import "./MovieListItem.css";
 
 function MovieListItem(props) {
-    const {title, poster_path} = props.movie;
-    const imageURL = `https://image.tmdb.org/t/p/w342/${poster_path}`
+    const {title, poster_path, vote_average, release_date} = props.movie;
+    const imageURL = `https://image.tmdb.org/t/p/w500/${poster_path}`
+    const releaseYear = release_date.substring(0, 4);
 
     return(
         <li className="movie-item">
-            <img src={imageURL} alt="" />
-            <span>{title}</span>
+            <img src={imageURL} alt="" />   
+            <div className="movie-description">
+                <h2>{title}</h2>
+                <section className="movie-details">
+                    <div className="movie-year">
+                        <span className="title">Year</span>
+                        <span>{releaseYear}</span>
+                    </div>
+                    <div className="movie-rating">
+                        <span className="title">Rating</span>
+                        <span>{vote_average}</span>
+                    </div>
+                </section>        
+            </div>
         </li>
     );
 }
