41228c8c0b46d8f182ee1c941910d33f43a0f9df
d4ydr34mzzz <d4y.dr34mzzz@gmail.com>
2019-06-02 13:39:07 -0700

    removed comments from Selection component

--- src/main/navigation/Selection.js
+++ src/main/navigation/Selection.js
@@ -4,14 +4,8 @@ import "./Selection.css";
 class Selection extends React.Component {
     constructor(props) {
         super(props);
-        // this.onGenreChange = this.onGenreChange.bind(this);
         this.handleChange = this.handleChange.bind(this);
-        // this.state = {genre: "Action"};
     }
-
-    // onGenreChange(event) {
-    //     this.setState({genre: event.target.value});
-    // }
 
     handleChange(event) {
         this.props.onGenreChange(event.target.value);
